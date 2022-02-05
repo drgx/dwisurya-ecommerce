381f45248ee941946eb9761185131b4e6a4464ed
Ryan Nixon Salim <ryan.nixon.salim@gmail.com>
2022-02-05 23:29:27 +0700

    test

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -49,8 +49,8 @@ class ControllerCommonMenu extends Controller {
 						);
 
 
-						if(!empty($childs['name'])) {
-							$image_category = $this->model_tool_image->resize($childs['name'], 200, 200);
+						if(!empty($child_2['name'])) {
+							$image_category = $this->model_tool_image->resize($child_2['name'], 200, 200);
 						}else{
 							$image_category = $this->config->get('config_url') . 'image/no_image.png';
 						}
