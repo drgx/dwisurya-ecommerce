d2a0831ba51087c6d3f5cc9768a60d1de90f2d2e
Ryan Nixon Salim <ryan.nixon.salim@gmail.com>
2022-04-05 12:30:25 +0700

    remove powerd

--- catalog/language/id-id/common/footer.php
+++ catalog/language/id-id/common/footer.php
@@ -14,4 +14,4 @@ $_['text_account']      = 'Akun Saya';
 $_['text_order']        = 'Riwayat Pesanan';
 $_['text_wishlist']     = 'Wish List';
 $_['text_newsletter']   = 'Newsletter';
-$_['text_powered']      = 'Powered By <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s';
+$_['text_powered']      = '%s &copy; %s';
